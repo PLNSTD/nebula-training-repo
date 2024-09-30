32db718f7949dfccd7b8505f59bdd511c65ca88c
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-09-30 18:43:09 -0400

    operators-homework file added and completed

---
+++ DAY 4/operators-homework.py
@@ -0,0 +1,17 @@
+## Homework
+# Write a Python script that:
+# - Calculates and prints the result of `(50 - 5*6) / 4`.
+# - Checks if `100` is not equal to `90` and prints the result.
+# - Uses `and`, `or`, and `not` to evaluate `((5 > 3) and (10 < 12)) or not(15 < 20)` and prints the outcome.
+
+output = (50 - 5 * 6) / 4
+
+print(output)
+
+bool_output = 100 != 90
+
+print (bool_output)
+
+and_or_not_ouytput = ((5 > 3) and (10 < 12)) or not(15 < 20)
+
+print(and_or_not_ouytput)
