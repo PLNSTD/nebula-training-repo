b6d25267f262b4579b3632ed0d41b827c0aea81d
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-12-18 22:09:26 -0500

    Inserting DB Into blog project

--- DAY 20/blog_project/blog_app.py
+++ DAY 20/blog_project/blog_app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 import json
 import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
 
@@ -8,6 +10,43 @@ db = {
     'current_post_id': 0,
     'posts': {}
 }
+
+DATABASE = {
+    'dbname': 'blog_db',
+    'user': 'postgres',
+    'password': 'admin',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+def get_db_connection():
+    return psycopg2.connect(**DATABASE)
+
+def data_transfer_to_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    for key, post in db["posts"].items():
+        cur.execute("INSERT INTO posts (title, content) VALUES (%s, %s);", (post["title"], post["content"]))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def initialize_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("""
+    DROP TABLE IF EXISTS posts;
+    CREATE TABLE posts (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(50),
+        content VARCHAR(200)
+    )
+     """)
+    # cur.execute("""INSERT INTO posts (title, content) VALUES ('Title3', 'My first DB post');""")
+    conn.commit()
+    cur.close()
+    conn.close()
+    data_transfer_to_db()
 
 if os.path.exists('db.json'):
     with open('db.json', 'r') as file:
@@ -20,7 +59,7 @@ def home():
 # Success request
 @app.route('/success')
 def success(data):
-    return jsonify({"status": "sucess", "data": data}), 200
+    return jsonify({"status": "success", "data": data}), 200
 
 # Error handler
 @app.errorhandler(400)
@@ -47,22 +86,33 @@ def get_posts():
     except ValueError:
         return bad_request("'page' and 'size' must be valid integers.")
     
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, title, content FROM posts;")
+    results = cur.fetchall()
+    cur.close()
+    conn.close()
+
     posts = []
+
+    for row in results:
+        posts.append({
+            "post_id": row[0],
+            "title": row[1],
+            "content": row[2],
+        })
 
     if query_title != 'default':
         print(query_title)
-        for key, value in db['posts'].items():
+        for value in posts:
             if query_title in value['title']:
                 posts.append(value)
 
     if query_content != 'default':
         print(query_content)
-        for key, value in db['posts'].items():
+        for value in posts:
             if query_content in value['content']:
                 posts.append(value)
-
-    if query_content == 'default' and query_title == 'default':
-        posts = list(db['posts'].values())
     
     # Pagination
     # 0, 1, 2, 3, 4
@@ -92,19 +142,40 @@ def create_post():
     new_post['post_id'] = db['current_post_id']
     db['posts'][new_post['post_id']] = new_post
 
-    save_db()
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO posts (title, content) VALUES (%s, %s);', (new_post['title'], new_post['content']))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    # save_db()
 
     return success(new_post)
 
 # 3. GET Post by ID
 @app.route('/posts/<int:id>')
 def get_post_by_id(id):
-    if str(id) not in db['posts']:
-        print(db)
-        return not_found('Post Not Found!')
 
-    target_post = db['posts'].get(str(id))
-    return success(target_post)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM posts WHERE id = %s;", (str(id)))
+    results = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    post = {
+        'id': results[0], 
+        'title': results[1],
+        'content': results[2]
+    }
+
+    # if str(id) not in db['posts']:
+    #     print(db)
+    #     return not_found('Post Not Found!')
+
+    # target_post = db['posts'].get(str(id))
+    return success(post)
     
 # 4. PUT Update a Post
 @app.route("/posts/<int:id>", methods=['PUT'])
@@ -140,4 +211,5 @@ def save_db():
         json.dump(db, file, indent=4)
 
 if __name__ == '__main__':
+    # initialize_db()
     app.run(debug=True)
---
+++ PythonGuide.md
@@ -0,0 +1,18 @@
+# PYTHON ESSENTIAL GUIDE
+
+### Dictionaries
+
+#### Methods
+
+- **Using `get(key, default)`** or **`[key]`**: To access or retrieve values in a dictionary.
+  ```python
+    email = my_dict.get('email', 'Not found')
+  ```
+- **`keys()`**: to retrieve all keys.
+- **`values()`**: To retrieve all values.
+- **`items()`**: To retrieve key-value pairs.
+- **`update({})`**: Inserts or updates (if exists) items in a dictionary as key-value pairs.
+  ```python
+    my_dict.update(other_dict)
+  ```
+  DAY 4
