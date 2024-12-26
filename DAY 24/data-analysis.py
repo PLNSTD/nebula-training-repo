074828bfe9049f8f7f9085f107240eef9fd2f6d4
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-12-25 23:19:13 -0500

    Ex 2

--- DAY 24/data-analysis.py
+++ DAY 24/data-analysis.py
@@ -9,23 +9,23 @@ np.random.seed(0)
     'Region': np.random.choice(['North', 'South', 'East', 'West'], size=100),
     'Sales': np.random.rand(100) * 1000,  # Sales figures between 0 and 1000
     'Transactions': np.random.randint(1, 100, size=100)  # Transactions between 1 and 100
-}
+}'''
 
 # Create DataFrame
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
+# df.to_csv('data_region.csv')
 
-df.to_csv('data_region.csv')'''
-
-# 1 Calculate the average sales and transactions per region
-df = pd.read_csv('example_data.csv')
+# Task 1.1 : Display the first 5 rows of the dataframe
+df = pd.read_csv('example_data.csv') # Load from file
 selected_by_position = df.iloc[0:5]
 print(selected_by_position)
 
-# **Calculate and display basic statistics for the 'Sales' and 'Transactions' columns.** Insert your code below:
+# Task 1.2: Caclulate and display basic statistics for Sales and Transactions columns
 transactions_details = df['Transactions'].describe()
 sales_details = df['Sales'].describe()
 print(pd.DataFrame({'Sales': transactions_details, 'Transactions': sales_details}))
 
+# Task 2.1: Calculate the average sales and transactions per region
 regions = df.groupby('Region')
 regions_sales_avg = regions['Sales'].mean()
 print(regions_sales_avg)
