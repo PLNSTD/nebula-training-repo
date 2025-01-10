01c145eb1d9dec9c27306a7c834d3b8459a32ff5
Plidher <65611683+PLNSTD@users.noreply.github.com>
2025-01-09 20:53:27 -0500

    Added Final project Overview and steps

--- DAY 30/pie_chart.py
+++ DAY 30/pie_chart.py
@@ -10,10 +10,13 @@ sales = df["Sales"]
 
 # Create a pie chart
 colors = ['#ff9999', '#66b3ff', '#99ff99', '#ffcc99']  # Custom colors
+'''for i in range(len(df['Sales'])):
+    if df['Sales'][i] > max_sales:
+        max_idx = i'''
 explode = (0.01, 0.05, 0.01, 0.01)  # "Exploding" the first slice
 
 plt.figure(figsize=(8, 8))
-plt.pie(sales, labels=regions, autopct='%1.1f%%', startangle=90, colors=colors, explode=explode, shadow=True)
+plt.pie(sales, labels=regions, autopct='%1.1f%%', startangle=180, colors=colors, explode=explode, shadow=True)
 plt.title("Enhanced Sales Distribution by Region")
 plt.savefig('customized_sales_pie_chart.png')
 plt.show()
